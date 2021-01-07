f20eac98bdc70b388f95c8399fd4ab65b0e2abef
Rifqi14 <rifqi.persie@gmail.com>
2021-01-07 09:58:38 +0700

    Workforce Group

---
+++ app/Http/Controllers/Admin/WorkforceGroupController.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\WorkforceGroup;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\View;
+
+class WorkforceGroupController extends Controller
+{
+    function __construct() {
+        View::share('menu_active', url('admin/workforcegroup'));
+        $this->middleware('accessmenu', ['except' => 'select']);
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.workforcegroup.index');
+    }
+
+    public function read(Request $request)
+    {
+        $start = $request->start;
+        $length = $request->length;
+        $query = $request->search['value'];
+        $sort = $request->columns[$request->order[0]['column']]['data'];
+        $dir = $request->order[0]['dir'];
+        $name = strtoupper($request->name);
+        $arsip = $request->category;
+
+        //Count Data
+        $query = WorkforceGroup::with('user')->whereRaw("upper(name) like '%$name%'");
+        if ($arsip) {
+            $query->onlyTrashed();
+        }
+        $recordsTotal = $query->count();
+
+        //Select Pagination
+        $query = WorkforceGroup::with('user')->whereRaw("upper(name) like '%$name%'");
+        if ($arsip) {
+            $query->onlyTrashed();
+        }
+        $query->offset($start);
+        $query->limit($length);
+        $query->orderBy($sort, $dir);
+        $results = $query->get();
+
+        $data = [];
+        foreach ($results as $result) {
+            $result->no = ++$start;
+            $data[] = $result;
+        }
+        return response()->json([
+            'draw' => $request->draw,
+            'recordsTotal' => $recordsTotal,
+            'recordsFiltered' => $recordsTotal,
+            'data' => $data
+        ], 200);
+    }
+
+    public function select(Request $request)
+    {
+        $start = $request->page ? $request->page - 1 : 0;
+        $length = $request->limit;
+        $name = strtoupper($request->name);
+
+        //Count Data
+        $query = WorkforceGroup::whereRaw("upper(name) like '%$name%'");
+        $recordsTotal = $query->count();
+
+        //Select Pagination
+        $query = WorkforceGroup::whereRaw("upper(name) like '%$name%'");
+        $query->offset($start);
+        $query->limit($length);
+        $results = $query->get();
+
+        $data = [];
+        foreach ($results as $result) {
+            $result->no = ++$start;
+            $data[] = $result;
+        }
+        return response()->json([
+            'total' => $recordsTotal,
+            'rows' => $data
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.workforcegroup.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'          => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'     => false,
+                'message'     => $validator->errors()->first()
+            ], 400);
+        }
+
+        try {
+            $workforce = WorkforceGroup::create([
+                'name'          => $request->name,
+                'data_manager'  => $request->data_manager ? 1 : 0,
+                'updated_by'    => Auth::id(),
+            ]);
+        } catch (QueryException $ex) {
+            return response()->json([
+                'status'      => false,
+                'message'     => $ex->errorInfo[2]
+            ], 400);
+        }
+        return response()->json([
+            'status'    => true,
+            'results'   => route('workforcegroup.index'),
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $workforce = WorkforceGroup::withTrashed()->find($id);
+        if ($workforce) {
+            return view('admin.workforcegroup.edit', compact('workforce'));
+        } else {
+            abort(404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'          => 'required',
+        ]);
+
+        if ($validator->fails()) {
+        	return response()->json([
+        		'status' 	=> false,
+        		'message' 	=> $validator->errors()->first()
+        	], 400);
+        }
+
+        $workforce = WorkforceGroup::withTrashed()->find($id);
+        $workforce->name = $request->name;
+        $workforce->data_manager = $request->data_manager ? 1 : 0;
+        $workforce->updated_by = Auth::id();
+        $workforce->save();
+        if (!$workforce) {
+            return response()->json([
+                'status' => false,
+                'message' 	=> $workforce
+            ], 400);
+        }
+        return response()->json([
+            'status' 	=> true,
+            'results'   => route('workforcegroup.index')
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $workforce_group = WorkforceGroup::find($id);
+            $workforce_group->delete();
+        } catch (QueryException $th) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Error archive data ' . $th->errorInfo[2]
+            ], 400);
+        }
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Success archive data'
+        ], 200);
+    }
+
+    public function restore($id)
+    {
+        try {
+            $workforce_group = WorkforceGroup::onlyTrashed()->find($id);
+            $workforce_group->restore();
+        } catch (QueryException $th) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Error restore data ' . $th->errorInfo[2]
+            ], 400);
+        }
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Success restore data'
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $workforce_group = WorkforceGroup::onlyTrashed()->find($id);
+            $workforce_group->forceDelete();
+        } catch (QueryException $th) {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Error delete data ' . $th->errorInfo[2]
+            ], 400);
+        }
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Success delete data'
+        ], 200);
+    }
+}
