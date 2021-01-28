9d56fb8d1dce2b7dcf4c778229da6ea8f4af3485
Jefri Rendra Wiratmaja <jefri.rendra@manajemenit.com>
2021-01-28 12:22:13 +0700

    Penaggung Jawab

--- app/Http/Controllers/Admin/GuarantorController.php
+++ app/Http/Controllers/Admin/GuarantorController.php
@@ -203,7 +203,7 @@ class GuarantorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Request $request,$id)
     {
         if(in_array('update',$request->actionmenu)){
             $guarantor = Guarantor::withTrashed()->find($id);
