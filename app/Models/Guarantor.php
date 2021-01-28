c81135895b2f29c7c25c644a51afa09d66f39185
Jefri Rendra Wiratmaja <jefri.rendra@manajemenit.com>
2021-01-28 12:20:26 +0700

    Penaggung Jawab

--- app/Models/Guarantor.php
+++ app/Models/Guarantor.php
@@ -13,7 +13,7 @@ class Guarantor extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'updated_by');
+        return $this->belongsTo(User::class, 'updated_by')s;
     }
     public function site()
     {
